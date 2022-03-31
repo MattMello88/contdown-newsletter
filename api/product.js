bb8562bc2c151f4454f10a181e2247e35186d9ef
MattMello88 <matheusnarciso@hotmail.com>
2022-03-31 15:00:38 -0300

    supabase

--- api/product.js
+++ api/product.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const { createClient } = require('@supabase/supabase-js')
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY);
+
 /**
  * GET product list.
  *
@@ -8,13 +13,31 @@ const router = express.Router();
  */
 router.get("/", async (req, res) => {
   try {
+    let { data: newslatter, error } = await supabase
+    .from('newslatter')
+    .select('*')
+
+    if (error){
+      res.json({
+        success: 'false',
+        status: 500,
+        message: error.message
+      })
+    } else {
+      res.json({
+        success: 'true',
+        status: 200,
+        message: "Consulta realizada com sucesso!",
+        data: newslatter
+      })
+    }
+
+  } catch (err) {
     res.json({
-      status: 200,
-      message: "Get data has successfully",
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send("Server error");
+      success: 'false',
+      status: 500,
+      message: err.message
+    })
   }
 });
 
