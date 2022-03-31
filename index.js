056e38b283f6a2536fa8c049c4ecca9153ed905e
MattMello88 <matheusnarciso@hotmail.com>
2022-03-31 15:11:04 -0300

    novo teste

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const serveStatic = require('serve-static');
+const cors = require("cors")
+const path = require('path');
 const app = express();
 const product = require("./api/product");
 
-app.use(express.json({ extended: false }));
+
+var corsOptions = {
+  origin: "*", // colocar aqui o ip externo e o nome do site
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+  preflightContinue: true,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.use("/api/product", product);
+
+app.use(serveStatic(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
